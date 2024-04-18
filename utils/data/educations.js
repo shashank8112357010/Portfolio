d03a0c1156aa625b4ec47821fa4a62e0c75e6dac
shashank8112357010 <shashanksharma1235999@gmail.com>
2024-04-18 12:19:14 +0530

    initial commit

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,13 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "217 - 2021 ",
+    institution: "Maharana Pratap Engineering College",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2015 - 2017",
+    institution: "Lucknow Public School",
   }
 ]
